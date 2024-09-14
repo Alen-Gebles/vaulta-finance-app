89c172b69c84be4ae14018a80f2c656fe58945fc
Zero <alengebles@gmail.com>
2024-09-14 21:37:51 +0200

    Made containers and layout for Budgets Section

--- app/path/budgets/page.jsx
+++ app/path/budgets/page.jsx
@@ -1,7 +1,44 @@
+'use client'
 import React from 'react'
+import './budgets.css'
+import { useEffect, useState } from 'react'
 
 export default function page() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('/data.json')
+    .then((response) => {
+      return response.json()
+    })
+    .then((jsonData) => {
+      setData(jsonData)
+    })
+    .catch((error) => {
+      console.error('Error fetching the json file', error)
+    })
+  }, [])
+
   return (
-    <div>Budgets</div>
+    <section className='budgetSection'>
+
+    <div className="budgetTopRow">
+      <h1>Budgets</h1>
+      <button>+ Add New Budget</button>
+    </div>
+
+    <div className='budgetHolder'>
+      <div className="spendingSummaryBox"></div>
+
+      <div className='categoryContainer'>
+          <div className='categoryBox'>Entertainment</div>
+          <div className='categoryBox'>Bills</div>
+          <div className='categoryBox'>Dining Out</div>
+          <div className='categoryBox'>Personal Care</div>
+      </div>
+    </div>
+      
+    </section>
   )
 }
