2ed8d0314709b3ae202fa152af263a5762d84d03
Zero <alengebles@gmail.com>
2024-09-23 21:37:51 +0200

    Finished Withdraw Money Part 🐸

--- app/path/pots/page.jsx
+++ app/path/pots/page.jsx
@@ -15,6 +15,7 @@ export default function Page() {
   const [addMoneyToPot, setAddMoneyToPot] = useState()
   const [money, setMoney] = useState()
   const [selectedPot, setSelectedPot] = useState(null);
+  const [withdrawMoneyToPot, setWithdrawMoneyToPot] = useState()
 
 
   useEffect(() => {
@@ -78,6 +79,12 @@ export default function Page() {
     setSelectedPot(pot);
     setAddMoneyToPot((prevState) => !prevState);
   };
+
+  const toggleWithdrawMoney = (pot) => {
+    setSelectedPot(pot);
+    setWithdrawMoneyToPot((prevState) => !prevState);
+  };
+  
   
 
   const handleInputChange = (event) => {
@@ -126,6 +133,19 @@ export default function Page() {
     setMoney('');
 }
   
+const withdrawMoney = () => {
+  const newAmount = parseFloat(money.trim());
+  if (isNaN(newAmount) || newAmount <= 0) return;
+
+  const newTotal = (selectedPot.total || 0) - newAmount;
+  setData((prevData) => {
+      const updatedPots = prevData.pots.map(pot => pot.name === selectedPot.name ? { ...pot, total: newTotal } : pot);
+      return { ...prevData, pots: updatedPots };
+  });
+
+  setWithdrawMoneyToPot(false);
+  setMoney('');
+}
 
   
 /*//////////////////////////////////////////////////////////////////////*/
@@ -176,7 +196,7 @@ export default function Page() {
 
           <div className="potTop">
               <div className="potDot" style={{ backgroundColor: pot.theme }}></div>
-              <h1>{pot.name}</h1>
+              <h1 className='potNameTop'>{pot.name}</h1>
               <button className='potEdit' onClick={() => toggleEditPopup(pot.name)}>&#183;&#183;&#183;</button>
               <div className={`editPopup ${activePopup === pot.name ? 'opacity-100' : 'opacity-0'}`}>
                 <button>Edit Pot</button>
@@ -197,7 +217,7 @@ export default function Page() {
           <div className={`dimScreen ${!addMoneyToPot ? 'hidden' : 'flex'}`}>
             <div className={`newBudget ${!addMoneyToPot ? 'opacity-0' : 'opacity-100'}`}>
               <div className='w-full flex justify-between items-center newHeadline'>
-                <h1>Add To Saving</h1>
+                <h1>Add To {selectedPot?.name}</h1>
                 <button className='newHeadlineBtn' onClick={() => toggleAddMoney()}>
                 <svg fill="#696868" height="64px" width="64px" version="1.1" id="Layer_1" viewBox="0 0 492 492"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872 c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872 c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052 L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116 c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952 c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116 c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"></path> </g> </g> </g></svg>
                 </button>
@@ -232,6 +252,45 @@ export default function Page() {
           </div>
           {/*///////*/}
 
+          {/* Withdraw Money */}
+          <div className={`dimScreen ${!withdrawMoneyToPot ? 'hidden' : 'flex'}`}>
+            <div className={`newBudget ${!withdrawMoneyToPot ? 'opacity-0' : 'opacity-100'}`}>
+              <div className='w-full flex justify-between items-center newHeadline'>
+                <h1>Withdraw From Saving</h1>
+                <button className='newHeadlineBtn'  onClick={() => toggleWithdrawMoney()}>
+                <svg fill="#696868" height="64px" width="64px" version="1.1" id="Layer_1" viewBox="0 0 492 492"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872 c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872 c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052 L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116 c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952 c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116 c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"></path> </g> </g> </g></svg>
+                </button>
+              </div>
+              <p className='text-gray-500 text-sm mt-3 mb-3'>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet.
+              </p>
+
+              <div className='potTotalSaved'>
+                <p className='potTotalSavedP'>New Amount</p>
+                <h2 className='potTotalSavedH2'>${selectedPot ? (selectedPot.total ? selectedPot.total.toFixed(2) : '0.00') : '0.00'}</h2>
+              </div>
+
+              <div className='potBarHolder'>
+                <div className='potBarHolderBcg'></div>
+                <div className='potBarHolderMain' style={{ backgroundColor: selectedPot?.theme, width: `${potBarWidth(selectedPot?.total || 0, selectedPot?.target || 0)}%` }}></div>
+              </div>
+
+              <div className='barInfoHolder'>
+                <p className='barInfoHolderP'>{selectedPot ? potBarWidth(selectedPot.total || 0, selectedPot.target || 0).toFixed(2) : '0.00'}%</p>
+                <p className='barInfoHolderP'>Target of ${selectedPot?.target || 0}</p>
+              </div>
+
+              <p className='budgetlabel'>Amount To Withdraw</p>
+              <input value={money}  onChange={handleMoneyAdding} onBlur={handleMoneyAdding}  className="categoryDropdown" type="text" placeholder='$ e.g. 400' />
+
+              <button className='addBudgetBtn' onClick={withdrawMoney}>
+                Confirm Addition
+              </button>
+
+            </div>
+          </div>
+          {/*////////////////////////*/}
+
           </div>
 
           <div className='potTotalSaved'>
@@ -251,7 +310,7 @@ export default function Page() {
 
           <div className='potBtnHolder'>
             <button className='potBtn'  onClick={() => toggleAddMoney(pot)}>+ Add Money</button>
-            <button className='potBtn'>Withdraw</button>
+            <button className='potBtn'  onClick={() => toggleWithdrawMoney(pot)}>Withdraw</button>
           </div>
 
         </div>
