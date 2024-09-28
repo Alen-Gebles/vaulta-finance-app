ae7a42a9eb23c0715732d44996cb478a824515b6
Zero <alengebles@gmail.com>
2024-09-28 20:46:34 +0200

    HOPEFULLY LAST CHANGE

--- app/page.jsx
+++ app/page.jsx
@@ -3,17 +3,28 @@ import Home from "./path/home/page";
 export default async function Main() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
   console.log("Base URL:", baseUrl); // Log base URL
-  const res = await fetch(`${baseUrl}/data.json`);
-  
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
+
+  try {
+    const res = await fetch(`${baseUrl}/data.json`);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await res.json();
+
+    return (
+      <main>
+        <Home data={data} />
+      </main>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <main>
+        <h1>Error fetching data</h1>
+        <p>{error.message}</p>
+      </main>
+    );
   }
-
-  const data = await res.json();
-
-  return (
-    <main>
-      <Home data={data} />
-    </main>
-  );
 }
