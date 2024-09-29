712fcf7248e0cee4fa70e194fa70e710a1939d0f
Zero <alengebles@gmail.com>
2024-09-29 19:58:55 +0200

    Added NEXT_PUBLIC_BASE_URL

--- app/data.jsx
+++ app/data.jsx
@@ -1,6 +1,6 @@
 // data.js
 export default async function getData() {
-  const res = await fetch('/data.json'); // Fetch from the public directory
+  const res = await fetch('https://alen-gebles.github.io/vaulta-finance-app/data.json'); // Fetch from the public directory
   
   if (!res.ok) {
     throw new Error('Failed to fetch data');
--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default async function Main() {
   console.log("Base URL:", baseUrl); // Log base URL
 
   try {
-    const res = await fetch(`${baseUrl}/data.json`);
+    const res = await fetch(`https://alen-gebles.github.io/vaulta-finance-app/data.json`);
 
     if (!res.ok) {
       throw new Error('Failed to fetch data');
